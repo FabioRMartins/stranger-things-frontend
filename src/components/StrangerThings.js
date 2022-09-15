4634e34d021e95014b22cb401e238c1592f2ce56
FabioMartins <fabbio1000@gmail.com>
2022-09-15 20:07:33 -0300

    teste2131212312

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -117,8 +117,7 @@ class StrangerThings extends React.Component {
       >
         <div className="content strangerfy">
           <div className="change-reality">
-            {process.env.REACT_APP_DEVELOPMENT === 'development'
-        && <h1>Em desenvolvimento</h1>}
+            { process.env.NODE_ENV && <h1>Em desenvolvimento</h1>}
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
               Mudar de Realidade
