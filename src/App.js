a8fad7a766cbfa02d5a28da7f8e83d13fef1a421
FabioMartins <fabbio1000@gmail.com>
2023-03-19 15:17:31 -0300

    vercel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const baseUrl = "https://stranger-things-backend.vercel.app/";
+
 function App() {
   return (
     <div className="App">
